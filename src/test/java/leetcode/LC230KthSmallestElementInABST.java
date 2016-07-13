2fa9418501765cf223a8b3913cd97749c9a25233
altermarkive <altermarkive@gmail.com>
2016-07-13 19:12:15 +0200

    Solved 22, 137, 230, 328, 337

---
+++ src/test/java/leetcode/LC230KthSmallestElementInABST.java
@@ -0,0 +1,87 @@
+package leetcode;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+/**
+ * https://leetcode.com/problems/kth-smallest-element-in-a-bst/
+ */
+public class LC230KthSmallestElementInABST {
+    public class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode(int x) {
+            val = x;
+        }
+    }
+
+    public class Solution {
+        public int kthSmallest(TreeNode root, int k, TreeNode counter) {
+            if (root.left != null) {
+                int result = kthSmallest(root.left, k, counter);
+                if (counter.val == k) {
+                    return result;
+                }
+            }
+            counter.val++;
+            if (counter.val == k) {
+                return root.val;
+            }
+            if (root.right != null) {
+                int result = kthSmallest(root.right, k, counter);
+                if (counter.val == k) {
+                    return result;
+                }
+            }
+            return 0;
+        }
+
+        public int kthSmallest(TreeNode root, int k) {
+            TreeNode counter = new TreeNode(0);
+            return kthSmallest(root, k, counter);
+        }
+    }
+
+    @Test
+    public void test_left() throws Exception {
+        TreeNode n1 = new TreeNode(1);
+        TreeNode n2 = new TreeNode(2);
+        TreeNode n3 = new TreeNode(3);
+        TreeNode n4 = new TreeNode(4);
+        n4.left = n3;
+        n3.left = n2;
+        n2.left = n1;
+        assertEquals(2, new Solution().kthSmallest(n4, 2));
+    }
+
+    @Test
+    public void test_right() throws Exception {
+        TreeNode n1 = new TreeNode(1);
+        TreeNode n2 = new TreeNode(2);
+        TreeNode n3 = new TreeNode(3);
+        TreeNode n4 = new TreeNode(4);
+        n1.right = n2;
+        n2.right = n3;
+        n3.right = n4;
+        assertEquals(2, new Solution().kthSmallest(n1, 2));
+    }
+
+    @Test
+    public void test_coverage() throws Exception {
+        TreeNode n1 = new TreeNode(1);
+        TreeNode n2 = new TreeNode(2);
+        TreeNode n3 = new TreeNode(3);
+        TreeNode n4 = new TreeNode(4);
+        TreeNode n5 = new TreeNode(5);
+        TreeNode n6 = new TreeNode(6);
+        n2.left = n1;
+        n2.right = n3;
+        n4.left = n2;
+        n4.right = n5;
+        n5.right = n6;
+        assertEquals(5, new Solution().kthSmallest(n4, 5));
+    }
+}
---
+++ src/test/java/leetcode/LC337HouseRobberIII.java
@@ -0,0 +1,78 @@
+package leetcode;
+
+import org.junit.Test;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import static org.junit.Assert.assertEquals;
+
+/**
+ * https://leetcode.com/problems/house-robber-iii/
+ */
+public class LC337HouseRobberIII {
+    public class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode(int x) {
+            val = x;
+        }
+    }
+
+    public class Solution {
+        public int rob(TreeNode root, Map<TreeNode, Integer> cache) {
+            Integer cached = cache.get(root);
+            if (cached != null) {
+                return cached;
+            }
+            if (root == null) return 0;
+            int with = root.val;
+            if (root.left != null) {
+                with += rob(root.left.left) + rob(root.left.right);
+            }
+            if (root.right != null) {
+                with += rob(root.right.left) + rob(root.right.right);
+            }
+            int without = rob(root.left) + rob(root.right);
+            int maximum = Math.max(with, without);
+            cache.put(root, cached);
+            return maximum;
+        }
+
+        public int rob(TreeNode root) {
+            return rob(root, new HashMap<>());
+        }
+    }
+
+    @Test
+    public void test_example_1() throws Exception {
+        TreeNode t3 = new TreeNode(3);
+        TreeNode l2 = new TreeNode(2);
+        TreeNode lr3 = new TreeNode(3);
+        TreeNode r3 = new TreeNode(3);
+        TreeNode rr1 = new TreeNode(1);
+        t3.left = l2;
+        t3.right = r3;
+        t3.left.right = lr3;
+        t3.right.right = rr1;
+        assertEquals(7, new Solution().rob(t3));
+    }
+
+    @Test
+    public void test_example_2() throws Exception {
+        TreeNode t3 = new TreeNode(3);
+        TreeNode l4 = new TreeNode(4);
+        TreeNode ll1 = new TreeNode(1);
+        TreeNode lr3 = new TreeNode(3);
+        TreeNode r5 = new TreeNode(5);
+        TreeNode rr1 = new TreeNode(1);
+        t3.left = l4;
+        t3.right = r5;
+        t3.left.left = ll1;
+        t3.left.right = lr3;
+        t3.right.right = rr1;
+        assertEquals(9, new Solution().rob(t3));
+    }
+}

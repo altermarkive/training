947c714acaf2350629ee2258dda5b4de2af0491f
altermarkive <altermarkive@gmail.com>
2016-07-21 19:24:39 +0200

    Improved 53 - O(1) space

--- src/test/java/leetcode/LC053MaximumSubarray.java
+++ src/test/java/leetcode/LC053MaximumSubarray.java
@@ -10,17 +10,13 @@ import static org.junit.Assert.assertEquals;
 public class LC053MaximumSubarray {
     public class Solution {
         public int maxSubArray(int[] nums) {
-            int[] sums = new int[nums.length];
-            int[] mins = new int[nums.length];
-            mins[0] = 0;
-            sums[0] = nums[0];
-            for (int i = 1; i < nums.length; i++) {
-                sums[i] = sums[i - 1] + nums[i];
-                mins[i] = sums[i - 1] < mins[i - 1] ? sums[i - 1] : mins[i - 1];
-            }
+            int sum = 0;
+            int min = 0;
             int max = Integer.MIN_VALUE;
             for (int i = 0; i < nums.length; i++) {
-                int delta = sums[i] - mins[i];
+                min = sum < min ? sum : min;
+                sum += nums[i];
+                int delta = sum - min;
                 max = delta > max ? delta : max;
             }
             return max;

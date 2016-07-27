15c784a2b80840603203f823d21fc314c038c1af
altermarkive <altermarkive@gmail.com>
2016-07-27 10:54:51 +0200

    Solved 377

---
+++ src/test/java/leetcode/LC377CombinationSumIV.java
@@ -0,0 +1,53 @@
+package leetcode;
+
+import org.junit.Test;
+
+import java.util.*;
+
+import static org.junit.Assert.assertEquals;
+
+/**
+ * https://leetcode.com/problems/combination-sum-iv/
+ */
+public class LC377CombinationSumIV {
+    public class Solution {
+        public int combinationSum4(int[] nums, int target, Map<Integer, Integer> cache) {
+            Integer cached = cache.get(target);
+            if (cached != null) {
+                return cached;
+            }
+            int sum = 0;
+            for (int value : nums) {
+                int candidate = target - value;
+                if (candidate > 0) {
+                    sum += combinationSum4(nums, candidate, cache);
+                }
+                if (candidate == 0) {
+                    sum++;
+                }
+                if (candidate < 0) {
+                    break;
+                }
+            }
+            cache.put(target, sum);
+            return sum;
+        }
+
+        public int combinationSum4(int[] nums, int target) {
+            Arrays.parallelSort(nums);
+            return combinationSum4(nums, target, new HashMap<>());
+        }
+    }
+
+    @Test
+    public void test_example() throws Exception {
+        int[] nums = {1, 2, 3};
+        assertEquals(7, new Solution().combinationSum4(nums, 4));
+    }
+
+    @Test
+    public void test_longer_example() throws Exception {
+        int[] nums = {4, 2, 1};
+        assertEquals(39882198, new Solution().combinationSum4(nums, 32));
+    }
+}

f1758363fdf2fd5ea5c57581df43df58d497d065
altermarkive <altermarkive@gmail.com>
2016-08-20 18:33:39 +0200

    Solved 274, 275, 331

---
+++ src/test/java/leetcode/LC275HIndexII.java
@@ -0,0 +1,41 @@
+package leetcode;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+/**
+ * https://leetcode.com/problems/h-index-ii/
+ */
+public class LC275HIndexII {
+    public class Solution {
+        public int hIndex(int[] citations) {
+            int n = citations.length;
+            int a = 0;
+            int z = n;
+            while (a < z) {
+                int m = (a + z) >> 1;
+                if (citations[m] == n - m) {
+                    return n - m;
+                } else if (citations[m] < n - m) {
+                    a = m + 1;
+                } else {
+                    z = m;
+                }
+            }
+            return n - a;
+        }
+    }
+
+    @Test
+    public void test_example() throws Exception {
+        int[] citations = {0, 1, 3, 5, 6};
+        assertEquals(3, new Solution().hIndex(citations));
+    }
+
+    @Test
+    public void test_none() throws Exception {
+        int[] citations = {0, 0, 0, 0, 0};
+        assertEquals(0, new Solution().hIndex(citations));
+    }
+}
---
+++ src/test/java/leetcode/LC331VerifyPreorderSerializationOfABinaryTree.java
@@ -0,0 +1,58 @@
+package leetcode;
+
+import org.junit.Test;
+
+import java.util.Stack;
+
+import static org.junit.Assert.assertEquals;
+
+/**
+ * https://leetcode.com/problems/verify-preorder-serialization-of-a-binary-tree/
+ */
+public class LC331VerifyPreorderSerializationOfABinaryTree {
+    public class Solution {
+        public boolean isValidSerialization(String preorder) {
+            if (null == preorder || preorder.length() == 0) return false;
+            String[] items = preorder.split(",");
+            Stack<Integer> kids = new Stack<>();
+            kids.push(1);
+            for (String node : items) {
+                while (kids.peek() == 2) {
+                    kids.pop();
+                    if (kids.size() == 0) return false;
+                }
+                kids.push(kids.pop() + 1);
+                if (!node.equals("#")) {
+                    kids.push(0);
+                }
+            }
+            while (kids.size() != 0 && kids.peek() == 2) kids.pop();
+            return kids.size() == 0;
+        }
+    }
+
+    @Test
+    public void test_empty() throws Exception {
+        assertEquals(true, new Solution().isValidSerialization("#"));
+    }
+
+    @Test
+    public void test_empty_but_not_really() throws Exception {
+        assertEquals(false, new Solution().isValidSerialization("#1"));
+    }
+
+    @Test
+    public void test_example_1() throws Exception {
+        assertEquals(true, new Solution().isValidSerialization("9,3,4,#,#,1,#,#,2,#,6,#,#"));
+    }
+
+    @Test
+    public void test_example_2() throws Exception {
+        assertEquals(false, new Solution().isValidSerialization("1,#"));
+    }
+
+    @Test
+    public void test_example_3() throws Exception {
+        assertEquals(false, new Solution().isValidSerialization("9,#,#,1"));
+    }
+}

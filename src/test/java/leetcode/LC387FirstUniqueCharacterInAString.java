e8b61836de5221fe28b7d7b530c462246822ce4c
altermarkive <altermarkive@gmail.com>
2016-08-22 23:30:19 +0200

    Solved 109, 167, 387

---
+++ src/test/java/leetcode/LC387FirstUniqueCharacterInAString.java
@@ -0,0 +1,48 @@
+package leetcode;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+/**
+ * https://leetcode.com/problems/first-unique-character-in-a-string/
+ */
+public class LC387FirstUniqueCharacterInAString {
+    public class Solution {
+        public int firstUniqChar(String s) {
+            int size = 'z' - 'a' + 1;
+            int[] count = new int[size];
+            int[] index = new int[size];
+            int length = s.length();
+            for (int i = length - 1; 0 <= i; i--) {
+                int key = s.charAt(i) - 'a';
+                index[key] = i;
+                count[key]++;
+            }
+            int min = -1;
+            for (int i = 0; i < size; i++) {
+                if (count[i] == 1) {
+                    if (min == -1 || index[i] < min) {
+                        min = index[i];
+                    }
+                }
+            }
+            return min;
+        }
+    }
+
+    @Test
+    public void test_leetcode() throws Exception {
+        assertEquals(0, new Solution().firstUniqChar("leetcode"));
+    }
+
+    @Test
+    public void test_loveleetcode() throws Exception {
+        assertEquals(2, new Solution().firstUniqChar("loveleetcode"));
+    }
+
+    @Test
+    public void test_empty() throws Exception {
+        assertEquals(-1, new Solution().firstUniqChar(""));
+    }
+}

081578a7722ccbb9d276022fc4541a0050967b5d
altermarkive <altermarkive@gmail.com>
2016-08-25 17:06:03 +0200

    Solved 372; Improved 5

--- src/test/java/leetcode/LC005LongestPalindromicSubstring.java
+++ src/test/java/leetcode/LC005LongestPalindromicSubstring.java
@@ -2,6 +2,9 @@ package leetcode;
 
 import org.junit.Test;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import static org.junit.Assert.assertEquals;
 
 /**
@@ -10,15 +13,16 @@ import static org.junit.Assert.assertEquals;
 public class LC005LongestPalindromicSubstring {
     public class Solution {
         private boolean isPalindrome(String s, int start, int finish) {
-            for (int i = start; i <= finish; i++) {
-                if (s.charAt(i) != s.charAt(finish - (i - start))) {
+            if (start < 0) return false;
+            while (start < finish) {
+                if (s.charAt(start++) != s.charAt(finish--)) {
                     return false;
                 }
             }
             return true;
         }
 
-        public String longestPalindrome(String s) {
+        public String longestPalindromeBrute(String s) {
             StringBuilder longest = new StringBuilder();
             for (int i = 0; i < s.length(); i++) {
                 for (int j = i + longest.length(); j < s.length(); j++) {
@@ -29,6 +33,64 @@ public class LC005LongestPalindromicSubstring {
                 }
             }
             return longest.toString();
+        }
+
+        private class TrieNode {
+            public Map<Character, TrieNode> next = new HashMap<>();
+        }
+
+        private String extendTrie(String part, TrieNode node, String longest) {
+            int length = part.length();
+            for (int i = 0; i < length; i++) {
+                char key = part.charAt(i);
+                TrieNode deeper = node.next.get(key);
+                if (null == deeper) {
+                    deeper = new TrieNode();
+                    node.next.put(key, deeper);
+                }else{
+                    String prefix = part.substring(0, i + 1);
+                    if (longest.length() < prefix.length() && isPalindrome(prefix, 0, prefix.length() - 1)) {
+                        longest = prefix;
+                    }
+                }
+                node = deeper;
+            }
+            return longest;
+        }
+
+        public String longestPalindromeWithTrie(String s) {
+            TrieNode root = new TrieNode();
+            int length = s.length();
+            for (int i = 0; i < length; i++) {
+                extendTrie(s.substring(i), root, "");
+            }
+            s = new StringBuilder(s).reverse().toString();
+            String longest = "";
+            for (int i = 0; i < length; i++) {
+                longest = extendTrie(s.substring(i), root, longest);
+
+            }
+            return longest;
+        }
+
+        public String longestPalindrome(String s) {
+            // Manacher algorithm
+            int a = 0;
+            int z = 0;
+            int size = 0;
+            int length = s.length();
+            for (int i = 0; i < length; i++) {
+                if (isPalindrome(s, i - size - 1, i)) {
+                    a = i - size - 1;
+                    z = i + 1;
+                    size += 2;
+                } else if (isPalindrome(s, i - size, i)) {
+                    a = i - size;
+                    z = i + 1;
+                    size++;
+                }
+            }
+            return s.substring(a, z);
         }
     }
 
---
+++ src/test/java/leetcode/LC372SuperPow.java
@@ -0,0 +1,63 @@
+package leetcode;
+
+import org.junit.Test;
+
+import java.util.LinkedList;
+import java.util.List;
+
+import static org.junit.Assert.assertEquals;
+
+/**
+ * https://leetcode.com/problems/super-pow/
+ */
+public class LC372SuperPow {
+    public class Solution {
+        private static final int MODULO_1337 = 1337;
+
+        private List<Integer> findPowerLoop(int value) {
+            List<Integer> modulos = new LinkedList<>();
+            boolean[] lut = new boolean[MODULO_1337];
+            int modulo = value;
+            while (!lut[modulo]) {
+                lut[modulo] = true;
+                modulos.add(modulo);
+                modulo = (modulo * value) % MODULO_1337;
+            }
+            return modulos;
+        }
+
+        private int modulo(int[] dividend, int divisor) {
+            int length = dividend.length;
+            int modulo = 0;
+            for (int i = 0; i < length; i++) {
+                modulo = (modulo * 10 + dividend[i]) % divisor;
+            }
+            return modulo;
+        }
+
+        public int superPow(int a, int[] b) {
+            // Assume: a = (1337 * n + m) where 0 <= m < 1337
+            // Then: a^b mod 1337 = (1337 * n + m)^n mod 1337 == m^b mod 1337
+            // This multiplication will cycle through certain "digits" of base 1337
+            // You can search for the loop by iterating
+            int m = a % MODULO_1337;
+            List<Integer> modulos = findPowerLoop(m);
+            // Get rid of loops from the power
+            int length = modulos.size();
+            int index = modulo(b, length);
+            // Look-up the power modulo
+            index = (index - 1 + length) % length;
+            return modulos.get(index);
+        }
+    }
+
+    @Test
+    public void test_2_3() throws Exception {
+        assertEquals(8, new Solution().superPow(2, new int[]{3}));
+    }
+
+    @Test
+    public void test_2_1_0() throws Exception {
+        assertEquals(1024, new Solution().superPow(2, new int[]{1, 0}));
+    }
+}

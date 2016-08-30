4a56f55c1883c795c6a460690e96844bbac8aa7f
altermarkive <altermarkive@gmail.com>
2016-08-30 14:10:32 +0200

    Improved - hackerrank: dijkstra short reach (java.util.Scanner was too slow)

--- src/test/java/hackerrank/dijkstrashortreach/Solution.java
+++ src/test/java/hackerrank/dijkstrashortreach/Solution.java
@@ -1,10 +1,7 @@
 package hackerrank.dijkstrashortreach;
 
-import java.io.InputStream;
-import java.io.PrintStream;
+import java.io.*;
 import java.util.*;
-import java.util.stream.Collectors;
-import java.util.stream.IntStream;
 
 /**
  * https://www.hackerrank.com/challenges/dijkstrashortreach
@@ -20,176 +17,116 @@ public class Solution {
         if (null == outOverride) {
             outOverride = System.out;
         }
-        Scanner scanner = new Scanner(inOverride);
-        int t = scanner.nextInt();
-        for (int i = 0; i < t; i++) {
-            int n = scanner.nextInt();
-            Graph graph = new Graph(n);
-            int m = scanner.nextInt();
-            for (int j = 0; j < m; j++) {
-                int x = scanner.nextInt() - 1;
-                int y = scanner.nextInt() - 1;
-                int r = scanner.nextInt();
-                graph.add(x, y, r);
+        BufferedReader reader = new BufferedReader(new InputStreamReader(inOverride));
+        try {
+            int t = Integer.parseInt(reader.readLine());
+            for (int i = 0; i < t; i++) {
+                String nm = reader.readLine();
+                int space = nm.indexOf(' ');
+                int n = Integer.parseInt(nm.substring(0, space));
+                Graph graph = new Graph(n);
+                int m = Integer.parseInt(nm.substring(space + 1));
+                for (int j = 0; j < m; j++) {
+                    String edge = reader.readLine();
+                    int firstSpace = edge.indexOf(' ');
+                    int secondSpace = edge.indexOf(' ', firstSpace + 1);
+                    int x = Integer.parseInt(edge.substring(0, firstSpace)) - 1;
+                    int y = Integer.parseInt(edge.substring(firstSpace + 1, secondSpace)) - 1;
+                    int r = Integer.parseInt(edge.substring(secondSpace + 1));
+                    graph.add(x, y, r);
+                }
+                int s = Integer.parseInt(reader.readLine()) - 1;
+                Vertex[] result = dijkstra(graph, s);
+                boolean first = true;
+                for (int j = 0; j < n; j++) {
+                    if (j == s) continue;
+                    if (first) {
+                        first = false;
+                    } else {
+                        outOverride.print(" ");
+                    }
+                    long value = result[j].distance;
+                    if (value == Long.MAX_VALUE) {
+                        outOverride.print(-1);
+                    } else {
+                        outOverride.print(value);
+                    }
+                }
+                outOverride.println();
             }
-            int s = scanner.nextInt() - 1;
-            int[] result = dijkstra(graph, n, s);
-            String string = IntStream.range(0, result.length).filter(k -> k != s).map(l -> result[l]).mapToObj(String::valueOf).collect(Collectors.joining(" "));
-            outOverride.println(string);
+        } catch (IOException ignored) {
         }
     }
 
     private static class Graph {
-        private static class Edge {
-            int node;
-            int weight;
-
-            Edge(int node, int weight) {
-                this.node = node;
-                this.weight = weight;
-            }
-        }
-
-        Map<Integer, Edge>[] adjacencies;
+        final Vertex[] vertices;
+        final int[][] lut;
 
         Graph(int n) {
-            adjacencies = new Map[n];
+            lut = new int[n][n];
+            vertices = new Vertex[n];
             for (int i = 0; i < n; i++) {
-                adjacencies[i] = new TreeMap<>();
+                vertices[i] = new Vertex();
+                Arrays.fill(lut[i], -1);
             }
-        }
-
-        Edge find(List<Edge> adjacent, int node) {
-            for (Edge edge : adjacent) {
-                if (edge.node == node) {
-                    return edge;
-                }
-            }
-            return null;
         }
 
         void add(int x, int y, int r) {
             if (x == y || r <= 0) return;
-            Map<Integer, Edge> adjacent;
-            Edge edge;
-            adjacent = adjacencies[x];
-            edge = adjacent.get(y);
-            if (null == edge) {
-                edge = new Edge(y, r);
-                adjacent.put(y, edge);
-            } else {
-                edge.weight = Math.min(edge.weight, r);
-            }
-            adjacent = adjacencies[y];
-            edge = adjacent.get(x);
-            if (null == edge) {
-                edge = new Edge(x, r);
-                adjacent.put(x, edge);
-            } else {
-                edge.weight = Math.min(edge.weight, r);
-            }
+            single(x, y, r);
+            single(y, x, r);
         }
 
-        Edge[][] cached() {
-            int n = adjacencies.length;
-            Edge[][] result = new Edge[n][0];
-            for (int i = 0; i < n; i++) {
-                Map<Integer, Edge> adjacency = adjacencies[i];
-                Set<Integer> adjacent = adjacency.keySet();
-                int m = adjacent.size();
-                result[i] = new Edge[m];
-                int j = 0;
-                for (int k : adjacent) {
-                    result[i][j++] = adjacency.get(k);
-                }
+        private void single(int a, int b, int r) {
+            Vertex vertex = vertices[a];
+            List<Edge> edges = vertex.edges;
+            Integer index = lut[a][b];
+            if (-1 == index) {
+                lut[a][b] = edges.size();
+                edges.add(new Edge(vertices[b], r));
+            } else {
+                Edge edge = edges.get(index);
+                edge.weight = Math.min(edge.weight, r);
             }
-            return result;
         }
     }
 
-    private static class DistanceComparator implements Comparator<Integer> {
-        private int[] distances;
-
-        public DistanceComparator(int[] distances) {
-            this.distances = distances;
-        }
+    static class Vertex implements Comparable<Vertex> {
+        long distance = Long.MAX_VALUE;
+        List<Edge> edges = new ArrayList<>();
 
         @Override
-        public int compare(Integer node1, Integer node2) {
-            if (distances[node1] < distances[node2]) return -1;
-            return 1;
+        public int compareTo(Vertex vertex) {
+            return (int) (distance - vertex.distance);
         }
     }
 
-    private static int minimum(Set<Integer> set) {
-        int result = Integer.MAX_VALUE;
-        for (int value : set) {
-            if (value < result) {
-                result = value;
-            }
+    private static class Edge {
+        Vertex vertex;
+        int weight;
+
+        Edge(Vertex vertex, int weight) {
+            this.vertex = vertex;
+            this.weight = weight;
         }
-        return result;
     }
 
-    private static int[] dijkstraUgh(Graph graph, int n, int s) {
-        int[] distances = new int[n];
-        Arrays.fill(distances, -1);
-        Set<Integer> unvisited = new HashSet<>();
-        unvisited.add(s);
-        distances[s] = 0;
+    private static Vertex[] dijkstra(Graph graph, int s) {
+        Vertex[] vertices = graph.vertices;
+        PriorityQueue<Vertex> unvisited = new PriorityQueue<>();
+        unvisited.add(vertices[s]);
+        vertices[s].distance = 0;
         while (unvisited.size() > 0) {
-            int node = minimum(unvisited);
-            unvisited.remove(node);
-            Map<Integer, Graph.Edge> adjacency = graph.adjacencies[node];
-            for (int next : adjacency.keySet()) {
-                int candidate = distances[node] + adjacency.get(next).weight;
-                if (-1 == distances[next] || candidate < distances[next]) {
-                    distances[next] = candidate;
-                    unvisited.add(next);
+            Vertex vertex = unvisited.poll();
+            for (Edge edge : vertex.edges) {
+                Vertex other = edge.vertex;
+                long candidate = vertex.distance + edge.weight;
+                if (candidate < other.distance) {
+                    other.distance = candidate;
+                    unvisited.add(other);
                 }
             }
         }
-        return distances;
-    }
-
-    private static int[] dijkstra(Graph graph, int n, int s) {
-        int[] distances = new int[n];
-        Arrays.fill(distances, -1);
-        SortedMap<Integer, Set<Integer>> unvisited = new TreeMap<>();
-        Set<Integer> entry = new TreeSet<>();
-        entry.add(s);
-        unvisited.put(0, entry);
-        distances[s] = 0;
-        while (unvisited.size() > 0) {
-            int minimum = unvisited.firstKey();
-            entry = unvisited.get(minimum);
-            int node = entry.iterator().next();
-            entry.remove(node);
-            if (entry.size() == 0) {
-                unvisited.remove(minimum);
-            }
-            Map<Integer, Graph.Edge> adjacency = graph.adjacencies[node];
-            for (int next : adjacency.keySet()) {
-                int candidate = distances[node] + adjacency.get(next).weight;
-                int old = distances[next];
-                if (-1 == old || candidate < old) {
-                    if (unvisited.containsKey(old)) {
-                        entry = unvisited.get(old);
-                        entry.remove(next);
-                        if (entry.size() == 0) {
-                            unvisited.remove(old);
-                        }
-                    }
-                    distances[next] = candidate;
-                    entry = unvisited.get(candidate);
-                    if (null == entry) {
-                        entry = new TreeSet<>();
-                        unvisited.put(candidate, entry);
-                    }
-                    entry.add(next);
-                }
-            }
-        }
-        return distances;
+        return vertices;
     }
 }

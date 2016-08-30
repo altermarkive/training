4951867b330cb97b8e0bd2225ff710b9ce23865f
altermarkive <altermarkive@gmail.com>
2016-08-30 14:43:21 +0200

    Generalized testing for Hacker Rank

--- src/test/java/hackerrank/dijkstrashortreach/TestSolution.java
+++ src/test/java/hackerrank/dijkstrashortreach/TestSolution.java
@@ -1,12 +1,7 @@
 package hackerrank.dijkstrashortreach;
 
+import hackerrank.SolutionTester;
 import org.junit.Test;
-
-import java.io.*;
-import java.lang.reflect.Field;
-import java.lang.reflect.Method;
-
-import static org.junit.Assert.assertEquals;
 
 /**
  * https://www.hackerrank.com/challenges/dijkstrashortreach
@@ -14,59 +9,26 @@ import static org.junit.Assert.assertEquals;
 public class TestSolution {
     @Test
     public void test_00() throws Exception {
-        Class solution = Solution.class;
-        String inName = "input00.txt";
-        String outName = "output00.txt";
-        test(solution, inName, outName);
+        SolutionTester.test(Solution.class, "input00.txt", "output00.txt");
     }
 
     @Test
     public void test_01() throws Exception {
-        Class solution = Solution.class;
-        String inName = "input01.txt";
-        String outName = "output01.txt";
-        test(solution, inName, outName);
+        SolutionTester.test(Solution.class, "input01.txt", "output01.txt");
     }
 
     @Test
     public void test_03() throws Exception {
-        Class solution = Solution.class;
-        String inName = "input03.txt";
-        String outName = "output03.txt";
-        test(solution, inName, outName);
+        SolutionTester.test(Solution.class, "input03.txt", "output03.txt");
     }
 
     @Test
     public void test_04() throws Exception {
-        Class solution = Solution.class;
-        String inName = "input04.txt";
-        String outName = "output04.txt";
-        test(solution, inName, outName);
+        SolutionTester.test(Solution.class, "input04.txt", "output04.txt");
     }
 
     @Test
     public void test_07() throws Exception {
-        Class solution = Solution.class;
-        String inName = "input07.txt";
-        String outName = "output07.txt";
-        test(solution, inName, outName);
-    }
-
-    private void test(Class solution, String inName, String outName) throws Exception {
-        Field modifiers = Field.class.getDeclaredField("modifiers");
-        modifiers.setAccessible(true);
-        Field inOverride = solution.getDeclaredField("inOverride");
-        inOverride.set(null, getClass().getResourceAsStream(inName));
-        Field outOverride = solution.getDeclaredField("outOverride");
-        ByteArrayOutputStream output = new ByteArrayOutputStream();
-        outOverride.set(null, new PrintStream(output));
-        Method main = solution.getDeclaredMethod("main", String[].class);
-        main.invoke(null, new Object[]{new String[]{}});
-        BufferedReader expected = new BufferedReader(new InputStreamReader(getClass().getResourceAsStream(outName)));
-        BufferedReader result = new BufferedReader(new InputStreamReader(new ByteArrayInputStream(output.toByteArray())));
-        while (expected.ready() && result.ready()) {
-            assertEquals(expected.readLine().trim(), result.readLine().trim());
-        }
-        assertEquals(expected.ready(), result.ready());
+        SolutionTester.test(Solution.class, "input07.txt", "output07.txt");
     }
 }

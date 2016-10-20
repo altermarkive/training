475a79eb90e3db4b8fd22ddb8d09496430a78457
altermarkive <altermarkive@gmail.com>
2016-10-20 12:35:18 +0200

    Solved - hackerrank: acm-acpc-team

---
+++ src/test/java/hackerrank/acmicpcteam/TestSolution.java
@@ -0,0 +1,14 @@
+package hackerrank.acmicpcteam;
+
+import hackerrank.SolutionTester;
+import org.junit.Test;
+
+/**
+ * https://www.hackerrank.com/challenges/acm-icpc-team
+ */
+public class TestSolution {
+    @Test
+    public void test_example() throws Exception {
+        SolutionTester.test(Solution.class, "input_example.txt", "output_example.txt");
+    }
+}

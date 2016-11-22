e1e660cd3a1806640d6bf3763ca700167e60c880
altermarkive <altermarkive@gmail.com>
2016-11-22 11:47:37 +0100

    Solved - hackerrank: word-order, most-commons, piling-up, py-collections-deque

---
+++ hackerrank/test_word_order.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+# https://www.hackerrank.com/challenges/word-order
+
+import collections
+
+
+def orderly(words):
+    unique = []
+    counted = collections.Counter()
+    for word in words:
+        if word not in counted:
+            unique.append(word)
+        counted[word] += 1
+    return unique, counted
+
+
+def main():
+    n = int(input().strip())
+    words = []
+    for i in range(n):
+        words.append(input().strip())
+    unique, counted = orderly(words)
+    print(len(unique))
+    print(' '.join([str(counted[word]) for word in unique]))
+
+if __name__ == '__main__':
+    main()

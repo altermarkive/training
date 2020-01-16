bb77b351b2c1e09bcd81e2ef526dacee98529558
altermarkive <altermarkive@gmail.com>
2020-01-16 17:14:12 +0100

    Switched to codecov.io

--- .github/workflows/python.tester.sh
+++ .github/workflows/python.tester.sh
@@ -1,8 +1,8 @@
-#!/bin/sh
+#!/bin/bash
 
 set -e
 
 pip3 --disable-pip-version-check install -r .github/workflows/python.tester.requirements.txt
 find algorithm-design -type d -exec touch {}/__init__.py \;
 coverage run -m unittest discover -s . -p "*.py"
-coveralls
+bash <(curl -s https://codecov.io/bash) -cF python

9d9ef5d9b8cf777918b6a4301a506acea01ccbf6
altermarkive <altermarkive@gmail.com>
2020-02-28 16:59:17 +0000

    Ported - hackerrank.com: acm-icpc-team

---
+++ algorithm-design/hackerrank/acm_icpc_team/acm_icpc_team.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python3
+# https://www.hackerrank.com/challenges/acm-icpc-team
+
+import unittest
+
+
+def acm_team(topic):
+    length = len(topic)
+    coverage_count = 0
+    teams_count = 0
+    for i in range(length - 1):
+        for j in range(i + 1, length):
+            count = 0
+            for pair in zip(topic[i], topic[j]):
+                count += 1 if '1' in pair else 0
+            if count > coverage_count:
+                coverage_count = count
+                teams_count = 1
+            elif count == coverage_count:
+                teams_count += 1
+    return (coverage_count, teams_count)
+
+
+class TestCode(unittest.TestCase):
+    def test_example_1(self):
+        topic = ['10101', '11100', '11010', '00101']
+        expected = (5, 2)
+        result = acm_team(topic)
+        self.assertEqual(result, expected)
+
+    def test_example_2(self):
+        topic = ['10101', '11110', '00010']
+        expected = (5, 1)
+        result = acm_team(topic)
+        self.assertEqual(result, expected)

dfc77c1c4692424a04314b3f816e379b1c618c1f
altermarkive <altermarkive@gmail.com>
2020-03-23 14:37:29 +0100

    Ported - hackerrank.com: bigger-is-greater

---
+++ algorithm-design/hackerrank/bigger_is_greater/bigger_is_greater.go
@@ -0,0 +1,30 @@
+package biggerisgreater
+
+// https://www.hackerrank.com/challenges/bigger-is-greater
+
+import (
+	"sort"
+)
+
+// BiggerIsGreater - implements the solution to the problem
+func BiggerIsGreater(w string) string {
+	array := []rune(w)
+	length := len(array)
+	for i := length - 1; 0 < i; i-- {
+		if array[i-1] < array[i] {
+			rest := array[i:]
+			sort.Slice(rest, func(k, l int) bool { return rest[k] < rest[l] })
+			for j := i; j < length; j++ {
+				if array[i-1] < array[j] {
+					exchange := array[i-1]
+					array[i-1] = array[j]
+					array[j] = exchange
+					rest := array[i:]
+					sort.Slice(rest, func(k, l int) bool { return rest[k] < rest[l] })
+					return string(array)
+				}
+			}
+		}
+	}
+	return "no answer"
+}
---
+++ algorithm-design/hackerrank/bigger_is_greater/bigger_is_greater.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python3
+# https://www.hackerrank.com/challenges/bigger-is-greater
+
+import unittest
+
+
+def bigger_is_greater(w):
+    array = list(w)
+    length = len(array)
+    for i in range(length - 1, 0, -1):
+        if array[i - 1] < array[i]:
+            array[i:] = sorted(array[i:])
+            for j in range(i, length):
+                if array[i - 1] < array[j]:
+                    exchange = array[i - 1]
+                    array[i - 1] = array[j]
+                    array[j] = exchange
+                    array[i:] = sorted(array[i:])
+                    return ''.join(array)
+    return 'no answer'
+
+
+class TestCode(unittest.TestCase):
+    def test_ab(self):
+        self.assertEqual('ba', bigger_is_greater('ab'))
+
+    def test_bb(self):
+        self.assertEqual('no answer', bigger_is_greater('bb'))
+
+    def test_hefg(self):
+        self.assertEqual('hegf', bigger_is_greater('hefg'))
+
+    def test_dhck(self):
+        self.assertEqual('dhkc', bigger_is_greater('dhck'))
+
+    def test_dkhc(self):
+        self.assertEqual('hcdk', bigger_is_greater('dkhc'))

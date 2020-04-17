50191ea8b8f8159bc7d584a99d08b08ced01961e
altermarkive <altermarkive@gmail.com>
2020-04-17 13:29:29 +0200

    Ported - hackerrank.com: staircase

---
+++ algorithm-design/hackerrank/staircase/staircase.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+# https://www.hackerrank.com/challenges/staircase
+
+import unittest
+
+
+def staircase(n):
+    result = []
+    for index in range(n):
+        line = ''
+        for i in range(n):
+            line += ' ' if i < n - 1 - index else '#'
+        result.append(line)
+    return result
+
+
+class TestCode(unittest.TestCase):
+    def test_example(self):
+        expected = [
+            '     #',
+            '    ##',
+            '   ###',
+            '  ####',
+            ' #####',
+            '######'
+        ]
+        self.assertEqual(expected, staircase(6))

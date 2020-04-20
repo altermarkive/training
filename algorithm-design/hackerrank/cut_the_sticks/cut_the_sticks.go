77b30a0ad9d4f22ed0c6dbcd5a47ada3b46a1509
altermarkive <altermarkive@gmail.com>
2020-04-20 16:19:31 +0200

    Corrected - hackerrank.com: cut-the-sticks

--- algorithm-design/hackerrank/cut_the_sticks/cut_the_sticks.go
+++ algorithm-design/hackerrank/cut_the_sticks/cut_the_sticks.go
@@ -3,13 +3,11 @@ package cavitymap
 // https://www.hackerrank.com/challenges/cut-the-sticks
 
 import (
-	"fmt"
 	"sort"
 )
 
 // CutTheSticks - implements the solution to the problem
 func CutTheSticks(arr []int32) []int32 {
-	fmt.Printf("%v\n", arr)
 	sort.Slice(arr, func(i, j int) bool { return arr[i] < arr[j] })
 	cuts := make([]int32, 0)
 	n := int32(len(arr))

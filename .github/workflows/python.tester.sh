2d5f1a3f4445adaec87fbbabb7588f938965cfa6
altermarkive <altermarkive@gmail.com>
2020-04-22 11:27:39 +0200

    Printing missed statements for Python coverage

--- .github/workflows/python.tester.sh
+++ .github/workflows/python.tester.sh
@@ -5,5 +5,5 @@ set -e
 pip3 --disable-pip-version-check install -r .github/workflows/python.tester.requirements.txt
 find algorithm-design -type d -exec touch {}/__init__.py \;
 coverage run -m unittest discover -s . -p "*.py"
-coverage report --omit="*/__init__.py"
+coverage report --show-missing --omit="*/__init__.py"
 coverage xml -i

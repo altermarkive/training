09b76755e88a65b2ef3c14a40d37c3885bb86aff
altermarkive <altermarkive@gmail.com>
2021-04-22 18:49:40 +0200

    Ported: leetcode.com/problems/roman-to-integer/

---
+++ algorithm-design/leetcode/lc013_roman_to_integer/LC013RomanToInteger.Tests.cs
@@ -0,0 +1,19 @@
+using Xunit;
+
+namespace AlgorithmDesign.leetcode.lc013_roman_to_integer
+{
+    public class SolutionTests
+    {
+        [Fact]
+        public void TestMcmliv()
+        {
+            Assert.Equal(1954, new Solution().RomanToInt("MCMLIV"));
+        }
+
+        [Fact]
+        public void TestNothing()
+        {
+            Assert.Equal(0, new Solution().RomanToInt(null));
+        }
+    }
+}
---
+++ algorithm-design/leetcode/lc013_roman_to_integer/lc013_roman_to_integer.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python3
+# https://leetcode.com/problems/roman-to-integer/
+
+import unittest
+
+
+class Solution:
+    def romanToInt(self, s: str) -> int:
+        if s is None:
+            return 0
+        result = 0
+        previous = 0
+        for i in range(len(s) - 1, -1, -1):
+            current = {
+                'I': 1,
+                'V': 5,
+                'X': 10,
+                'L': 50,
+                'C': 100,
+                'D': 500,
+                'M': 1000}[s[i]]
+            current = -current if current < previous else current
+            result += current
+            previous = current
+        return result
+
+
+class TestCode(unittest.TestCase):
+    def test_MCMLIV(self):
+        self.assertEqual(Solution().romanToInt('MCMLIV'), 1954)
+
+    def test_nothing(self):
+        self.assertEqual(Solution().romanToInt(None), 0)

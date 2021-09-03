fb3805d4e3aef860fb46c6b0557b168217c4964a
altermarkive <altermarkive@gmail.com>
2021-09-03 19:39:12 +0200

    Ported: leetcode.com/problems/remove-nth-node-from-end-of-list/

---
+++ algorithm-design/leetcode/lc019_remove_nth_node_from_end_of_list/LC019RemoveNthNodeFromEndOfList.Tests.cs
@@ -0,0 +1,32 @@
+using Xunit;
+
+namespace AlgorithmDesign.leetcode.lc019_remove_nth_node_from_end_of_list
+{
+    public class SolutionTests
+    {
+        [Fact]
+        public void test_1_2__1_1()
+        {
+            ListNode n1 = new ListNode(1);
+            ListNode n2 = new ListNode(2);
+            n1.next = n2;
+            n2.next = null;
+            ListNode n = n1;
+            Solution solution = new Solution();
+            Assert.Equal(1, n.val);
+            Assert.Equal(2, n.next.val);
+            n = solution.RemoveNthFromEnd(n, 1);
+            Assert.Equal(1, n.val);
+            n = solution.RemoveNthFromEnd(n, 1);
+            Assert.Null(n);
+        }
+
+        [Fact]
+        public void test_null__0()
+        {
+            Solution solution = new Solution();
+            ListNode n = solution.RemoveNthFromEnd(null, 0);
+            Assert.Null(n);
+        }
+    }
+}
---
+++ algorithm-design/leetcode/lc019_remove_nth_node_from_end_of_list/lc019_remove_nth_node_from_end_of_list.py
@@ -0,0 +1,57 @@
+#!/usr/bin/env python3
+# https://leetcode.com/problems/remove-nth-node-from-end-of-list/
+
+import unittest
+
+from typing import Optional
+
+
+class ListNode:
+    def __init__(self, val=0, following=None):
+        self.val = val
+        self.next = following
+
+
+class Solution:
+    # pylint: disable=C0301
+    def removeNthFromEnd(self, head: Optional[ListNode], n: int) -> Optional[ListNode]:  # noqa
+        # Translate the index number from counted
+        # from the back to a one counted from the front
+        node = head
+        while node is not None:
+            n -= 1
+            node = node.next
+        n = -n
+        # Do the deletion
+        node = ListNode(0)
+        node.next = head
+        head = node
+        while head is not None:
+            if n == 0:
+                if head.next is not None:
+                    head.next = head.next.next
+                    break
+            head = head.next
+            n -= 1
+        return node.next
+
+
+class TestCode(unittest.TestCase):
+    def test_1_2__1_1(self):
+        n1 = ListNode(1)
+        n2 = ListNode(2)
+        n1.next = n2
+        n2.next = None
+        n = n1
+        solution = Solution()
+        self.assertEqual(n.val, 1)
+        self.assertEqual(n.next.val, 2)
+        n = solution.removeNthFromEnd(n, 1)
+        self.assertEqual(n.val, 1)
+        n = solution.removeNthFromEnd(n, 1)
+        self.assertEqual(n, None)
+
+    def test_none_0(self):
+        solution = Solution()
+        n = solution.removeNthFromEnd(None, 0)
+        self.assertEqual(n, None)

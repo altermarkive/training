bc13dc0344c2e6dce345500a0376ea4636e1efec
altermarkive <altermarkive@gmail.com>
2021-09-06 11:37:01 +0200

    Ported: leetcode.com/problems/valid-parentheses

---
+++ algorithm-design/leetcode/lc020_valid_parentheses/LC020ValidParentheses.cs
@@ -0,0 +1,49 @@
+// https://leetcode.com/problems/valid-parentheses/
+
+using System.Collections.Generic;
+
+namespace AlgorithmDesign.leetcode.lc020_valid_parentheses
+{
+    public class Solution
+    {
+        public bool IsValid(string s)
+        {
+            if (s == null) return false;
+            if (s.Length == 0) return true;
+            Stack<char> stack = new Stack<char>();
+            foreach (char character in s)
+            {
+                switch (character)
+                {
+                    case '(':
+                    case '{':
+                    case '[':
+                        stack.Push(character);
+                        break;
+                    case ')':
+                        if (stack.Count == 0 || stack.Peek() != '(')
+                        {
+                            return false;
+                        }
+                        stack.Pop();
+                        break;
+                    case '}':
+                        if (stack.Count == 0 || stack.Peek() != '{')
+                        {
+                            return false;
+                        }
+                        stack.Pop();
+                        break;
+                    case ']':
+                        if (stack.Count == 0 || stack.Peek() != '[')
+                        {
+                            return false;
+                        }
+                        stack.Pop();
+                        break;
+                }
+            }
+            return stack.Count == 0;
+        }
+    }
+}
---
+++ algorithm-design/leetcode/lc020_valid_parentheses/lc020_valid_parentheses.py
@@ -0,0 +1,58 @@
+#!/usr/bin/env python3
+# https://leetcode.com/problems/valid-parentheses/
+
+import unittest
+
+
+class Solution:
+    def isValid(self, s: str) -> bool:
+        if s is None:
+            return False
+        if len(s) == 0:
+            return True
+        stack = []
+        for character in s:
+            if character in ['(', '{', '[']:
+                stack.append(character)
+            elif character == ')':
+                if len(stack) == 0 or stack[-1] != '(':
+                    return False
+                stack.pop()
+            elif character == '}':
+                if len(stack) == 0 or stack[-1] != '{':
+                    return False
+                stack.pop()
+            elif character == ']':
+                if len(stack) == 0 or stack[-1] != '[':
+                    return False
+                stack.pop()
+            return len(stack) == 0
+
+
+class TestCode(unittest.TestCase):
+    def test_RE(self):
+        self.assertEqual(Solution().isValid(')'), False)
+
+    def test_RB_RE(self):
+        self.assertEqual(Solution().isValid('()'), True)
+
+    def test_RB_RE_SB_SE_CB_CE(self):
+        self.assertEqual(Solution().isValid('()[]{}'), True)
+
+    def test_RB_SB_RE_SE(self):
+        self.assertEqual(Solution().isValid('([)]'), False)
+
+    def test_RB_CB_RE_CE(self):
+        self.assertEqual(Solution().isValid('({)}'), False)
+
+    def test_SB_RB_SE_RE(self):
+        self.assertEqual(Solution().isValid('[(])'), False)
+
+    def test_RE__SE__CE(self):
+        self.assertEqual(Solution().isValid(')'), False)
+        self.assertEqual(Solution().isValid(']'), False)
+        self.assertEqual(Solution().isValid('}'), False)
+
+    def test_nothing(self):
+        self.assertEqual(Solution().isValid(None), False)
+        self.assertEqual(Solution().isValid(''), True)

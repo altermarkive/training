f5f90d4c9999ae55e1fec4bda07716f7a15f2448
altermarkive <altermarkive@gmail.com>
2021-09-06 12:14:12 +0200

    Ported: leetcode.com/problems/valid-parentheses

--- algorithm-design/leetcode/lc020_valid_parentheses/lc020_valid_parentheses.py
+++ algorithm-design/leetcode/lc020_valid_parentheses/lc020_valid_parentheses.py
@@ -26,7 +26,7 @@ class Solution:
                 if len(stack) == 0 or stack[-1] != '[':
                     return False
                 stack.pop()
-            return len(stack) == 0
+        return len(stack) == 0
 
 
 class TestCode(unittest.TestCase):

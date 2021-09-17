fe506b1a70d75aac72fa47af6c4a9e0911ea91b1
altermarkive <altermarkive@gmail.com>
2021-09-17 10:04:58 +0200

    Ported: leetcode.com/problems/remove-duplicates-from-sorted-array

---
+++ algorithm-design/leetcode/lc026_remove_duplicates_from_sorted_array/LC026RemoveDuplicatesFromSortedArray.Tests.cs
@@ -0,0 +1,18 @@
+using System.Linq;
+using Xunit;
+
+namespace AlgorithmDesign.leetcode.lc026_remove_duplicates_from_sorted_array
+{
+    public class SolutionTests
+    {
+        [Fact]
+        public void test_1_2_2_3_4_4_7()
+        {
+            int[] nums1 = { 1, 2, 2, 3, 4, 4, 7 };
+            int length = new Solution().RemoveDuplicates(nums1);
+            Assert.Equal(5, length);
+            int[] expected = { 1, 2, 3, 4, 7 };
+            Assert.Equal(expected, nums1.Take(length));
+        }
+    }
+}
---
+++ algorithm-design/leetcode/lc026_remove_duplicates_from_sorted_array/lc026_remove_duplicates_from_sorted_array.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+# https://leetcode.com/problems/remove-duplicates-from-sorted-array/
+
+import unittest
+
+from typing import List
+
+
+class Solution:
+    def removeDuplicates(self, nums: List[int]) -> int:
+        counter = 0
+        for i in range(1, len(nums)):
+            spot = counter
+            if nums[i] == nums[i - 1 - spot]:
+                counter += 1
+            nums[i - spot] = nums[i]
+        return len(nums) - counter
+
+
+class TestCode(unittest.TestCase):
+    def test_1_2_2_3_4_4_7(self):
+        nums1 = [1, 2, 2, 3, 4, 4, 7]
+        length = Solution().removeDuplicates(nums1)
+        self.assertEqual(length, 5)
+        expected = [1, 2, 3, 4, 7]
+        self.assertListEqual(expected, nums1[0:length])

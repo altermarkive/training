83f39789eda94a86ea40a082fac77f48e15c0e28
altermarkive <altermarkive@gmail.com>
2021-09-20 15:32:15 +0200

    Ported: leetcode.com/problems/implement-strstr

---
+++ algorithm-design/leetcode/lc028_implement_str_str/lc028_implement_str_str.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3
+# https://leetcode.com/problems/implement-strstr/
+
+import unittest
+
+
+class Solution:
+    def strStr(self, haystack: str, needle: str) -> int:
+        if len(haystack) < len(needle):
+            return -1
+        for i in range(1 + len(haystack) - len(needle)):
+            if haystack[i:i + len(needle)] == needle:
+                return i
+        return -1
+
+
+class TestCode(unittest.TestCase):
+    def test_empty(self):
+        self.assertEqual(Solution().strStr('', ''), 0)
+
+    def test_mississippi_a(self):
+        self.assertEqual(Solution().strStr('mississippi', 'a'), -1)
+
+    def test_mississippi_si(self):
+        self.assertEqual(Solution().strStr('mississippi', 'si'), 3)
+
+    def test_bigger_in_smaller(self):
+        self.assertEqual(Solution().strStr('', 'si'), -1)

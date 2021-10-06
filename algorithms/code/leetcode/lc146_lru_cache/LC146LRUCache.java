5b8e780015157b2c370bc7bb58851f0fa927cbe2
altermarkive <altermarkive@gmail.com>
2021-10-06 16:29:39 +0200

    Python: leetcode.com/problems/lru-cache

---
+++ algorithms/code/leetcode/lc146_lru_cache/LC146LRUCache.java
@@ -0,0 +1,88 @@
+package leetcode.lc146_lru_cache;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * https://leetcode.com/problems/lru-cache/ #medium
+ */
+public final class LC146LRUCache {
+    private LC146LRUCache() {
+    }
+
+    private static class Node {
+        private Node preceding = null;
+        private Node following = null;
+        protected int key;
+        protected int value;
+
+        Node(final int keyValue, final int valueValue) {
+            key = keyValue;
+            value = valueValue;
+        }
+    }
+
+    public static class LRUCache {
+        private int capacity;
+        private Map<Integer, Node> lut = new HashMap<>();
+        private Node head;
+        private Node tail;
+
+        LRUCache(final int capacityValue) {
+            capacity = capacityValue;
+            head = new Node(0, 0);
+            tail = new Node(0, 0);
+            head.following = tail;
+            tail.preceding = head;
+        }
+
+        /**
+         * Get element from the LRU
+         *
+         * @param key Identifies the element
+         * @return Value associated with the key
+         */
+        public int get(final int key) {
+            int value = -1;
+            if (lut.containsKey(key)) {
+                value = remove(key);
+                insert(key, value);
+            }
+            return value;
+        }
+
+        /**
+         * Set element in the LRU
+         *
+         * @param key   Identifies the element
+         * @param value Value to be associated with the key
+         */
+        public void set(final int key, final int value) {
+            if (lut.containsKey(key)) {
+                remove(key);
+            } else {
+                if (lut.size() >= capacity) {
+                    remove(tail.preceding.key);
+                }
+            }
+            insert(key, value);
+        }
+
+        private void insert(final int key, final int value) {
+            Node node = new Node(key, value);
+            node.preceding = head;
+            node.following = head.following;
+            node.preceding.following = node;
+            node.following.preceding = node;
+            lut.put(key, node);
+        }
+
+        private int remove(final int key) {
+            Node node = lut.get(key);
+            lut.remove(key);
+            node.preceding.following = node.following;
+            node.following.preceding = node.preceding;
+            return node.value;
+        }
+    }
+}
---
+++ algorithms/code/leetcode/lc146_lru_cache/lc146_lru_cache.py
@@ -0,0 +1,72 @@
+#!/usr/bin/env python3
+# https://leetcode.com/problems/lru-cache/
+
+import unittest
+
+
+class Node:
+    def __init__(self, key, value):
+        self.key = key
+        self.value = value
+        self.preceding = None
+        self.following = None
+
+
+class LRUCache:
+    def __init__(self, capacity: int):
+        self.capacity = capacity
+        self.lut = {}
+        self.head = Node(0, 0)
+        self.tail = Node(0, 0)
+        self.head.following = self.tail
+        self.tail.preceding = self.head
+
+    def get(self, key: int) -> int:
+        value = -1
+        if key in self.lut:
+            value = self.remove(key)
+            self.insert(key, value)
+        return value
+
+    def put(self, key: int, value: int) -> None:
+        if key in self.lut:
+            self.remove(key)
+        else:
+            if len(self.lut) >= self.capacity:
+                self.remove(self.tail.preceding.key)
+        self.insert(key, value)
+
+    def insert(self, key: int, value: int) -> None:
+        node = Node(key, value)
+        node.preceding = self.head
+        node.following = self.head.following
+        node.preceding.following = node
+        node.following.preceding = node
+        self.lut[key] = node
+
+    def remove(self, key: int) -> int:
+        node = self.lut[key]
+        del self.lut[key]
+        node.preceding.following = node.following
+        node.following.preceding = node.preceding
+        return node.value
+
+
+class TestCode(unittest.TestCase):
+    def test_example(self):
+        lru = LRUCache(2)
+        lru.put(1, 1)
+        lru.put(2, 2)
+        self.assertEqual(lru.get(1), 1)
+        lru.put(3, 3)
+        self.assertEqual(lru.get(2), -1)
+        lru.put(4, 4)
+        self.assertEqual(lru.get(1), -1)
+        self.assertEqual(lru.get(3), 3)
+        self.assertEqual(lru.get(4), 4)
+
+    def test_repeated_put_same(self):
+        lru = LRUCache(1)
+        lru.put(1, 1)
+        lru.put(1, 1)
+        self.assertEqual(lru.get(1), 1)

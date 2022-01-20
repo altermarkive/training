19cb7987a09875972594448a00019c527967464b
altermarkive <altermarkive@gmail.com>
2022-01-20 10:28:43 +0100

    Solved: GeeksForGeeks - Sort a string according to the order defined by another string

---
+++ algorithms/code/geeksforgeeks/sort_string_according_order_defined_another_string/sort_string_according_order_defined_another_string.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python3
+# https://www.geeksforgeeks.org/sort-string-according-order-defined-another-string/
+# #google
+
+import unittest
+
+
+class Solution:
+    def sortByPattern(self, string, pat):
+        # Build a decorating hash table
+        lut = {}
+        for i, key in enumerate(pat):
+            lut[key] = i
+        # Reorder
+        chars = sorted(string, key=lambda character: lut.get(character, -1))
+        return ''.join(chars)
+
+
+class TestCode(unittest.TestCase):
+    def test_example_1(self):
+        self.assertEqual(
+            'bca',
+            Solution().sortByPattern('abc', 'bca'))
+
+    def test_example_2(self):
+        self.assertEqual(
+            'bbbcccaaa',
+            Solution().sortByPattern('abcabcabc', 'bxyzca'))
+
+    def test_example_3(self):
+        self.assertEqual(
+            'codijak',
+            Solution().sortByPattern('jcdokai', 'wcyuogmlrdfphitxjakqvzbnes'))

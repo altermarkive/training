3e23a0f24ffa02cc8c4934215dab67ed0278010d
altermarkive <altermarkive@gmail.com>
2024-07-16 16:21:35 +0200

    Linting

--- algorithms/code/leetcode/lc739_daily_temperatures/lc739_daily_temperatures.py
+++ algorithms/code/leetcode/lc739_daily_temperatures/lc739_daily_temperatures.py
@@ -21,17 +21,15 @@ class TestCode(unittest.TestCase):
     def test_example_1(self):
         self.assertListEqual(
             [1, 1, 4, 2, 1, 1, 0, 0],
-            Solution().dailyTemperatures([73, 74, 75, 71, 69, 72, 76, 73])
+            Solution().dailyTemperatures([73, 74, 75, 71, 69, 72, 76, 73]),
         )
 
     def test_example_2(self):
         self.assertListEqual(
-            [1, 1, 1, 0],
-            Solution().dailyTemperatures([30, 40, 50, 60])
+            [1, 1, 1, 0], Solution().dailyTemperatures([30, 40, 50, 60])
         )
 
     def test_example_3(self):
         self.assertListEqual(
-            [1, 1, 0],
-            Solution().dailyTemperatures([30, 60, 90])
+            [1, 1, 0], Solution().dailyTemperatures([30, 60, 90])
         )

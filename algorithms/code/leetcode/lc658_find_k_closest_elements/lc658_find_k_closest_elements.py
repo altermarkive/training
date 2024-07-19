8823d59495268dfda6a64832ddd29518e84f4f8e
altermarkive <altermarkive@gmail.com>
2024-07-19 17:23:24 +0200

    Implemented LC658

---
+++ algorithms/code/leetcode/lc658_find_k_closest_elements/lc658_find_k_closest_elements.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python3
+# https://leetcode.com/problems/find-k-closest-elements/
+
+import unittest
+from typing import List
+
+
+class Solution:
+    def findClosestElements(self, arr: List[int], k: int, x: int) -> List[int]:
+        begin, end = 0, len(arr) - 1 - k
+        while begin <= end:
+            middle = (begin + end) // 2
+            if x - arr[middle] <= arr[middle + k] - x:
+                end = middle - 1
+            else:
+                begin = middle + 1
+        return arr[begin:begin + k]
+
+
+class TestCode(unittest.TestCase):
+    def test_example_1(self):
+        self.assertListEqual(
+            [1, 2, 3, 4], Solution().findClosestElements([1, 2, 3, 4, 5], 4, 3)
+        )
+
+    def test_example_2(self):
+        self.assertListEqual(
+            [1, 2, 3, 4],
+            Solution().findClosestElements([1, 2, 3, 4, 5], 4, -1),
+        )
+
+    def test_other_1(self):
+        self.assertListEqual(
+            [3, 3, 4],
+            Solution().findClosestElements(
+                [0, 0, 1, 2, 3, 3, 4, 7, 7, 8], 3, 5
+            ),
+        )

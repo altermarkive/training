86d3c48ff9eca3faaff931a7cb882a6581237c72
altermarkive <altermarkive@gmail.com>
2024-07-22 17:24:37 +0200

    Implemented LC389

---
+++ algorithms/code/leetcode/lc389_find_the_difference/find-the-difference.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+# https://leetcode.com/problems/find-the-difference/
+
+import unittest
+from collections import Counter
+
+
+class Solution:
+    def findTheDifference(self, s: str, t: str) -> str:
+        count_t = Counter(t)
+        count_s = Counter(s)
+        result = ''
+        for key, count in count_t.items():
+            if count_s[key] != count:
+                result = key
+                break
+        return result
+
+
+class TestCode(unittest.TestCase):
+    def test_example_1(self):
+        self.assertListEqual(
+            'e', Solution().findTheDifference('abcd', 'abcde')
+        )
+
+    def test_example_2(self):
+        self.assertListEqual('e', Solution().findTheDifference('', 'y'))

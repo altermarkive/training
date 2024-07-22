8147870ae87a21b59a92faa8f109be82873277b1
altermarkive <altermarkive@gmail.com>
2024-07-22 18:38:59 +0200

    Implemented LC392

---
+++ algorithms/code/leetcode/lc392_is_subsequence/lc392_is_subsequence.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python3
+# https://leetcode.com/problems/is-subsequence/
+
+import unittest
+
+
+class Solution:
+    def isSubsequence(self, s: str, t: str) -> bool:
+        n = len(s)
+        if n == 0:
+            return True
+        if n == 1:
+            return s in t
+        character = s[0]
+        at = t.find(character)
+        if at != -1:
+            return self.isSubsequence(s[1:], t[at + 1:])
+        return False
+
+
+class TestCode(unittest.TestCase):
+    def test_example_1(self):
+        self.assertTrue(Solution().isSubsequence('abc', 'ahbgdc'))
+
+    def test_example_2(self):
+        self.assertFalse(Solution().isSubsequence('axc', 'ahbgdc'))
+
+    def test_other_1(self):
+        self.assertTrue(Solution().isSubsequence('', 'ahbgdc'))
+
+    def test_other_2(self):
+        self.assertFalse(Solution().isSubsequence('aaaaaa', 'eeaaa'))

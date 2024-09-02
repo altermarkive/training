f1c74cfd2dce896a93020e79fca58b53ac531696
altermarkive <altermarkive@gmail.com>
2024-09-02 16:23:45 +0200

    tarpaulin

--- .github/workflows/rust.tester.get.sh
+++ .github/workflows/rust.tester.get.sh
@@ -1,3 +1,5 @@
 #!/usr/bin/env sh
 
 set -e
+
+cargo install cargo-tarpaulin
--- .github/workflows/rust.tester.run.sh
+++ .github/workflows/rust.tester.run.sh
@@ -6,5 +6,6 @@ export CARGO_INCREMENTAL=0
 export RUSTFLAGS='-Ccodegen-units=1 -Cinline-threshold=0 -Clink-dead-code -Coverflow-checks=off'
 export RUSTDOCFLAGS='-Ccodegen-units=1 -Cinline-threshold=0 -Clink-dead-code -Coverflow-checks=off'
 rm -rf Cargo.lock target
-cargo test  # -vv
+cargo test
+cargo tarpaulin
 find . -name mod.rs -exec rm {} \;

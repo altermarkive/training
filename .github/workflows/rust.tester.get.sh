3a8410c4cc17a975d0f83561d6a04b0e8539f59d
altermarkive <altermarkive@gmail.com>
2024-09-02 17:39:59 +0200

    Corrected Rust coverage scripts

--- .github/workflows/rust.tester.get.sh
+++ .github/workflows/rust.tester.get.sh
@@ -3,4 +3,4 @@
 set -e
 
 rustup component add llvm-tools-preview
-curl -L https://github.com/mozilla/grcov/releases/latest/download/grcov-x86_64-unknown-linux-gnu.tar.bz2 | tar jxf -
+cargo install grcov
--- .github/workflows/rust.tester.run.sh
+++ .github/workflows/rust.tester.run.sh
@@ -7,5 +7,7 @@ export RUSTFLAGS='-Ccodegen-units=1 -Cinline-threshold=0 -Clink-dead-code -Cover
 export RUSTDOCFLAGS='-Ccodegen-units=1 -Cinline-threshold=0 -Clink-dead-code -Coverflow-checks=off'
 rm -rf Cargo.lock target
 LLVM_PROFILE_FILE="./target/coverage/training.profraw" cargo test
-./grcov ./target/coverage --binary-path ./target/debug/ -s . -o lcov.info --keep-only . --output-types lcov
 find . -name mod.rs -exec rm {} \;
+grcov ./target/coverage --binary-path ./target/debug/ -s . -o . --ignore-not-existing --keep-only 'algorithms/*' --output-types lcov,files
+cat files
+mv lcov lcov.info

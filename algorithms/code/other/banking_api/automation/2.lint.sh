8aea79540b1c6230f095efe3a32c59a65ea955ad
altermarkive <altermarkive@gmail.com>
2024-09-04 20:59:06 +0200

    Linting

--- algorithms/code/other/banking_api/automation/2.lint.sh
+++ algorithms/code/other/banking_api/automation/2.lint.sh
@@ -5,4 +5,4 @@ set -e
 SELF=$(dirname "$0")
 BASE=$(realpath "$SELF/..")
 
-docker run --rm -it -v $BASE:/tests -w /tests --entrypoint /bin/sh banking -c 'pip install -e .[tests] && /bin/sh automation/2.linters.sh'
+docker run --rm -it -v "$BASE:/tests" -w /tests --entrypoint /bin/sh banking -c 'pip install -e .[tests] && /bin/sh automation/2.linters.sh'

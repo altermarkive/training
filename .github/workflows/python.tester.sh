c413022e31a09760528717b96ae91e16da71a68b
altermarkive <altermarkive@users.noreply.github.com>
2025-04-21 16:56:34 +0200

    Updated python.tester.sh to ignore report errors on generated/tmp files

--- .github/workflows/python.tester.sh
+++ .github/workflows/python.tester.sh
@@ -3,6 +3,6 @@
 set -e
 
 uv run --with-requirements .github/workflows/python.environment.requirements.txt --no-project coverage run -m unittest discover -s . -p "*.py"
-uv run --with-requirements .github/workflows/python.environment.requirements.txt --no-project coverage report --show-missing --omit="*/__init__.py"
+uv run --with-requirements .github/workflows/python.environment.requirements.txt --no-project coverage report --show-missing --omit="*/__init__.py" -i
 uv run --with-requirements .github/workflows/python.environment.requirements.txt --no-project coverage xml -i
 (find . -name __pycache__ -exec rm -rf {} \; 2>/dev/null) || true

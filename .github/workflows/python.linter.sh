e4aacf7e680b2b5511380ca534ef3dbf7a8f8394
altermarkive <altermarkive@gmail.com>
2026-02-16 14:53:17 +0100

    Adjusted Python depencencies file name for Dependabot

--- .github/workflows/python.linter.sh
+++ .github/workflows/python.linter.sh
@@ -3,17 +3,17 @@
 set -e
 
 echo "--- ruff ---"
-uv run --with-requirements .github/workflows/python.environment.requirements.txt --no-project ruff check --select I,E,B,SIM algorithms/code
-uv run --with-requirements .github/workflows/python.environment.requirements.txt --no-project ruff format --check --diff --config .github/linters/.ruff.toml algorithms/code
+uv run --with-requirements .github/workflows/requirements.txt --no-project ruff check --select I,E,B,SIM algorithms/code
+uv run --with-requirements .github/workflows/requirements.txt --no-project ruff format --check --diff --config .github/linters/.ruff.toml algorithms/code
 echo "--- ty ---"
-uv run --with-requirements .github/workflows/python.environment.requirements.txt --no-project ty check algorithms/code
+uv run --with-requirements .github/workflows/requirements.txt --no-project ty check algorithms/code
 echo "--- flake8 ---"
-uv run --with-requirements .github/workflows/python.environment.requirements.txt --no-project flake8 --ignore=E203,W503
+uv run --with-requirements .github/workflows/requirements.txt --no-project flake8 --ignore=E203,W503
 echo "--- pylint ---"
-uv run --with-requirements .github/workflows/python.environment.requirements.txt --no-project sh -c "find . -name '*.py' | grep -v banking_api | xargs pylint --disable=C0103,C0111,C0302,R0801,R0903"
+uv run --with-requirements .github/workflows/requirements.txt --no-project sh -c "find . -name '*.py' | grep -v banking_api | xargs pylint --disable=C0103,C0111,C0302,R0801,R0903"
 echo "--- mypy ---"
-uv run --with-requirements .github/workflows/python.environment.requirements.txt --no-project mypy algorithms --exclude banking_api
+uv run --with-requirements .github/workflows/requirements.txt --no-project mypy algorithms --exclude banking_api
 echo "--- pyright ---"
-uv run --with-requirements .github/workflows/python.environment.requirements.txt --no-project pyright algorithms
+uv run --with-requirements .github/workflows/requirements.txt --no-project pyright algorithms
 echo "--- bandit ---"
-uv run --with-requirements .github/workflows/python.environment.requirements.txt --no-project bandit --skip B101 -r .
+uv run --with-requirements .github/workflows/requirements.txt --no-project bandit --skip B101 -r .

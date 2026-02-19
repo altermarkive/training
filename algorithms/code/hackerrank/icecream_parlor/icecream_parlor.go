c4c9cd6ff9bd4a25315dae3d3f4f74fa70f24ddc
altermarkive <altermarkive@gmail.com>
2026-02-19 17:18:47 +0100

    Index type adjustments to avoid conversion issues

--- algorithms/code/hackerrank/icecream_parlor/icecream_parlor.go
+++ algorithms/code/hackerrank/icecream_parlor/icecream_parlor.go
@@ -3,22 +3,21 @@ package icecreamparlor
 
 // IcecreamParlor - implements the solution to the problem
 func IcecreamParlor(m int32, arr []int32) []int32 {
-	n := int32(len(arr))
 	mapped := make(map[int32]int32)
-	for i := range n {
-		mapped[arr[i]] = i
+	idx := int32(0)
+	for _, v := range arr {
+		mapped[v] = idx
+		idx++
 	}
-	for i := range n {
-		first := i
-		value := m - arr[i]
-		_, valueInMapped := mapped[value]
-		if valueInMapped {
-			second := mapped[value]
-			if first == second {
-				continue
-			}
+	idx = 0
+	for _, v := range arr {
+		first := idx
+		value := m - v
+		second, valueInMapped := mapped[value]
+		if valueInMapped && first != second {
 			return []int32{first + 1, second + 1}
 		}
+		idx++
 	}
 	return []int32{0, 0}
 }

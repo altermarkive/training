28d19c45f2cb8368a46be50da9410e7c53f12fdd
altermarkive <altermarkive@gmail.com>
2026-02-23 18:57:01 +0100

    Synced Go versions

--- .github/workflows/golang.linter.sh
+++ .github/workflows/golang.linter.sh
@@ -2,13 +2,13 @@
 
 set -e
 
-go install github.com/golangci/golangci-lint/v2/cmd/golangci-lint@v2.10.1
+go install github.com/golangci/golangci-lint/v2/cmd/golangci-lint@latest
 
 cd algorithms
 gofmt -d .
 golangci-lint run --config ../.github/linters/.golangci.yml
-if [ -n "$(go fix -diff ./...)" ]; then
-	go fix -diff ./...
+if [ -n "$(go fix ./...)" ]; then
+	go fix ./...
 	exit 1
 fi
 go vet ./...
